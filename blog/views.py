064131ec192b846710c3cf5bfdb4f3f4fe84af14
chdan-hub <ksmii358@gmail.com>
2025-09-17 21:37:46 -0700

    day-3.3

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from http.client import responses
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from blog.forms import BlogForm
 from blog.models import Blog
+from django.urls import reverse
 
 
 def blog_list(request):
@@ -19,6 +21,18 @@ def blog_detail(request, pk):
     context = {'blog': blog}
 
     return render(request, 'blog_detail.html', context)
+
+def blog_create(request):
+    form = BlogForm(request.POST or None)
+    if form.is_valid():
+        blog = form.save(commit=False)
+        blog.author = request.user
+        blog.save()
+        return redirect(reverse('blog_detail', kwargs={'pk': blog.pk}))
+
+
+    context = {'form': form}
+    return render(request, 'blog_create.html', context)
 
     # visits = int(request.COOKIES.get('visits', 0)) + 1
     #
