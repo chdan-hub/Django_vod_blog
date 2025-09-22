86a243ced076af9d4c2bd4b2ea3f8f581e5a5852
chdan-hub <ksmii358@gmail.com>
2025-09-22 01:01:36 -0700

    day-6 add Comments Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from blog.models import Blog
+from blog.models import Blog, Comment
+from utils.models import TimestampModel
+
+admin.site.register(Comment)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['content', 'author']
+    extra = 1
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    ...
+    inlines = [
+        CommentInline
+    ]
