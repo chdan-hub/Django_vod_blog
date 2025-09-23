01bf554f307b4f47b23bf21e9aae1e45bd7c6d00
chdan-hub <ksmii358@gmail.com>
2025-09-22 19:07:16 -0700

    day-6 add Comment pagination

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', cb_views.BlogListView.as_view(), name='list'),
-    path('<int:pk>/', cb_views.BlogDetailView.as_view(), name='detail'),
+    path('<int:blog_pk>/', cb_views.BlogDetailView.as_view(), name='detail'),
     path('create/', cb_views.BlogCreateView.as_view(), name='create'),
     path('<int:pk>/updata/', cb_views.BlogUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', cb_views.BlogDeleteView.as_view(), name='delete'),
