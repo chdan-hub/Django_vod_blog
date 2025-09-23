e0c7b117dfaedab796c60eb7a76fdd283769f5a2
chdan-hub <ksmii358@gmail.com>
2025-09-23 01:14:34 -0700

    day-7 add Thumbnail Image

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+from pathlib import Path
+from PIL import Image
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -20,6 +23,7 @@ class Blog(TimestampModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     image = models.ImageField('이미지', null=True, blank=True, upload_to='blog/%Y/%m/%d')
+    thumbnail = models.ImageField('썸네일', null=True, blank=True, upload_to='blog/%Y/%m/%d/thumbnail')
     # models.CASCADE => 같이 삭제
     # models.PROTECT => 삭제가 불가능함(유저를 삭제하려고 할 떄 블로그가 있으면 유저 삭제가 불가능)
     # models.SET_NULL => 널 값을 넣습니다 => 유저 삭제 시 블로그의 author가 null이 됨
@@ -32,6 +36,43 @@ class Blog(TimestampModel):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('blog:detail', kwargs={'blog_pk': self.pk})
+
+    def get_thumbnail_image_url(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        elif self.image:
+            return self.image.url
+        return None
+
+    def save(self, *args, **kwargs):
+        if not self.image:
+            return super().save(*args, **kwargs)
+
+        image = Image.open(self.image)
+        image.thumbnail((300,300))
+        image_path = Path(self.image.name)
+
+        thumbnail_name = image_path.stem
+        thumbnail_extenstion = image_path.suffix.lower()
+        thumbnail_filename = f'{thumbnail_name}_thumb{thumbnail_extenstion}'
+
+        if thumbnail_extenstion in ['.jpg', '.jpeg']:
+            file_type = 'JPEG'
+        elif thumbnail_extenstion == '.gif':
+            file_type = 'GIF'
+        elif thumbnail_extenstion == '.png':
+            file_type = 'PNG'
+        else:
+            return super().save(*args, **kwargs)
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, file_type)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumbnail_filename, temp_thumb, save=False)
+        temp_thumb.close()
+        return super().save(*args, **kwargs)
+
 
     class Meta:
         verbose_name = '블로그'
